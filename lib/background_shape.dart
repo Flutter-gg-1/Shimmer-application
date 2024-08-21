bd6a1c94484969b2eb46b8ef2d843892b20ae789
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-21 16:58:15 +0300

    ADD HomeScreen

---
+++ lib/background_shape.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class BackgroundShape extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Path path_0 = Path();
+    path_0.moveTo(61.6642, 0);
+    path_0.lineTo(77.0802, 33.3333);
+    path_0.cubicTo(92.4963, 66.6667, 123.328, 133.333, 136.175, 200);
+    path_0.cubicTo(149.022, 266.667, 143.883, 333.333, 136.175, 400);
+    path_0.cubicTo(128.467, 466.667, 118.19, 533.333, 115.62, 600);
+    path_0.cubicTo(113.051, 666.667, 118.19, 733.333, 120.759, 766.667);
+    path_0.lineTo(123.328, 800);
+    path_0.lineTo(0, 800);
+    path_0.lineTo(0, 766.667);
+    path_0.cubicTo(0, 733.333, 0, 666.667, 0, 600);
+    path_0.cubicTo(0, 533.333, 0, 466.667, 0, 400);
+    path_0.cubicTo(0, 333.333, 0, 266.667, 0, 200);
+    path_0.cubicTo(0, 133.333, 0, 66.6667, 0, 33.3333);
+    path_0.lineTo(0, 0);
+    path_0.lineTo(61.6642, 0);
+    path_0.close();
+
+    Paint paint_0_fill = Paint()..style = PaintingStyle.fill;
+    paint_0_fill.color = Color(0xffFFCECE).withOpacity(1.0);
+    canvas.drawPath(path_0, paint_0_fill);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:assignment_14/background_shape.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,8 +6,29 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
+    return Scaffold(
+      bottomNavigationBar: Container(
+        height: 70,
+        color: Colors.white,
+      ),
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Stack(
+          children: [
+            const SizedBox(height: 70),
+            const Positioned(top: 70, left: 150, child: Text('Home Screen')),
+            Positioned(
+              left: 0,
+              top: 0,
+              child: CustomPaint(
+                size: const Size(144, 800),
+                painter: BackgroundShape(),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
