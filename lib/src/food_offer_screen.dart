dbcd46dea577d9c65357975115aa47f1ea534cdf
Basel-75 <basel_1422@outlook.com>
2024-08-21 23:42:49 +0300

    Update Project assest & dependencies

---
+++ lib/src/food_offer_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FoodOfferScreen extends StatefulWidget {
+  const FoodOfferScreen({super.key});
+
+  @override
+  State<FoodOfferScreen> createState() => _FoodOfferScreenState();
+}
+
+class _FoodOfferScreenState extends State<FoodOfferScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+    );
+  }
+}
